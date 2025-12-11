99a32d0c0f1672306dbadde3215ba6be85567cd4
laksharajjha <laksharajjha@gmail.com>
2025-12-11 17:51:36 +0530

    wiring up mongodb connection, using motor for async

---
+++ app/db/database.py
@@ -0,0 +1,20 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.core.config import settings
+
+class Database:
+    client: AsyncIOMotorClient = None
+
+    def connect(self):
+        self.client = AsyncIOMotorClient(settings.MONGO_URL)
+
+    def close(self):
+        if self.client:
+            self.client.close()
+
+    def get_db(self):
+        return self.client.master_db
+
+    def get_dynamic_collection(self, collection_name: str):
+        return self.client[collection_name]
+
+db = Database()
